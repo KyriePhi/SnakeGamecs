0993124fcfabc8036d5198ee8da4d0c64fdc1c53
Mehmet Berat ŞENEL <107311601+KyriePhi@users.noreply.github.com>
2022-08-31 16:55:44 +0300

    Add files via upload

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(426, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(398, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(344, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Level :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Game Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(455, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Created by Mehmet Berat Şenel";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(465, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 15);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "mehmetberat.com";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(456, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(190, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Snake Control : A, W, S, D keys";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,179 @@
+﻿using SnakeGame.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+          /* Designing and Programming :     */
+         /*  Mehmet Berat ŞENEL             */
+        /*   This project under cc license */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static bool movexp=false, moveyp=false, movexn=false, moveyn = false;
+        private static int Length;
+        private static int Score;
+        private static Panel[] panels= new Panel[100];
+        private static int Foodx,Foody;
+        private static Panel Food = new Panel();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label4);
+            for (int i = 0; i < Length; i++)
+            {
+                panel1.Controls.Add(panels[i]);
+            }
+            NextLevel();
+            FoodCreator();
+            for (int i = Length; i > 1; i--)
+            {
+                Panel panell = panels[i - 1];
+                Panel panelll = panels[i - 2];
+                panell.Location = new System.Drawing.Point(panelll.Location.X, panelll.Location.Y);
+            }
+            if (moveyn)
+            {
+                Panel panel = panels[0];
+                panel.Location = new System.Drawing.Point(panel.Location.X, panel.Location.Y + 15);
+                panels[0] = panel;
+            }
+            else if (moveyp)
+            {
+                Panel panel = panels[0];
+                panel.Location = new System.Drawing.Point(panel.Location.X, panel.Location.Y - 15);
+                panels[0] = panel;
+            }
+            else if (movexp)
+            {
+                Panel panel = panels[0];
+                panel.Location = new System.Drawing.Point(panel.Location.X + 15, panel.Location.Y);
+                panels[0] = panel;
+            }
+            else if (movexn)
+            {
+                Panel panel = panels[0];
+                panel.Location = new System.Drawing.Point(panel.Location.X - 15, panel.Location.Y);
+                panels[0] = panel;
+            }
+        }
+        private void SnakeBody(int name,int locationx,int locationy)
+        {
+            Panel panel = new Panel();
+            panel.BackgroundImage = global::SnakeGame.Properties.Resources.body;
+            panel.Location = new System.Drawing.Point(locationx, locationy);
+            panel.Name = name.ToString();
+            panel.Size = new System.Drawing.Size(10, 10);
+            panel1.Controls.Add(panel);
+            panels[name] = panel;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            panel1.Controls.Clear();
+            Random rnd = new Random();
+            int locx = rnd.Next(10, 410);
+            int locy = rnd.Next(10, 410);
+            Foodx = rnd.Next(10, 410);
+            Foody = rnd.Next(10, 410);
+            SnakeBody(0,locx,locy);
+            Length = 1;
+            FoodCreator();
+            moveyp = false;
+            moveyn = false;
+            movexp = false;
+            movexn = false;
+            timer1.Start();
+            Score = 0;
+            label2.Text = Score.ToString();
+            label3.Text = Length.ToString();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W)
+            {
+                moveyp = true;
+                moveyn = false;
+                movexp = false;
+                movexn = false;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                movexp = true;
+                moveyn = false;
+                moveyp = false;
+                movexn = false;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                moveyn = true;
+                moveyp = false;
+                movexp = false;
+                movexn = false;
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                movexn = true;
+                moveyn = false;
+                movexp = false;
+                moveyp = false;
+            }
+        }
+        private void FoodCreator()
+        {
+            Food.BackColor = Color.IndianRed;
+            Food.Location = new System.Drawing.Point(Foodx, Foody);
+            Food.Name = "Food";
+            Food.Size = new System.Drawing.Size(5, 5);
+            panel1.Controls.Add(Food);
+        }
+        private void NextLevel()
+        {
+            Panel panel = panels[0];
+            if (( panel.Location.X >= Foodx-9 ) & ( panel.Location.X <= Foodx+8))
+            {
+                if (( panel.Location.Y >= Foody-8) & ( panel.Location.Y <= Foody + 9))
+                {
+                    Random rnd = new Random();
+                    Foodx = rnd.Next(10, 410);
+                    Foody = rnd.Next(10, 410);
+                    if (movexp)
+                    {
+                        SnakeBody(Length, panel.Location.X-(15*Length), panel.Location.Y);
+                    }
+                    else if (movexn)
+                    {
+                        SnakeBody(Length, panel.Location.X + (15 * Length), panel.Location.Y);
+                    }
+                    else if (moveyp)
+                    {
+                        SnakeBody(Length, panel.Location.X, panel.Location.Y - (15 * Length));
+                    }
+                    else if (moveyn)
+                    {
+                        SnakeBody(Length, panel.Location.X, panel.Location.Y + (15 * Length));
+                    }
+                    Length += 1;
+                    Score += Length * 10;
+                    label2.Text = Score.ToString();
+                    label3.Text = Length.ToString();
+                }
+            }
+        }
+    }
+}
